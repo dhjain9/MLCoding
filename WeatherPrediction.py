067f03a7bc7cbb76bbd40f089a7a22f54734853a
dhjain9 <dhjain@cisco.com>
2023-12-04 07:46:33 -0800

    Add files via upload
    Weather prediction with relu function

---
+++ WeatherPrediction.py
@@ -0,0 +1,38 @@
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
+
+# Generate random weather data for demonstration
+np.random.seed(40)
+data_size = 10000
+features = np.random.rand(data_size, 5)  # 5 input features for each data point
+labels = np.random.rand(data_size, 1)  # 1 output label for each data point (e.g., temperature)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+
+# Standardize the input data
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Build a simple neural network model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(10, activation='relu', input_shape=(5,)),
+    tf.keras.layers.Dense(1)  # Output layer with a single neuron for regression
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Train the model
+model.fit(X_train_scaled, y_train, epochs=50, batch_size=32, validation_split=0.1)
+
+# Evaluate the model on the test set
+y_pred = model.predict(X_test_scaled)
+
+# Calculate and print the Mean Squared Error
+mse = mean_squared_error(y_test, y_pred)
+print(f'Mean Squared Error on Test Set: {mse}')
