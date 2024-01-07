5f511e7d2c26b1c297d54c89e50ad9393c2924cd
d9jain <dhanendra.ietf@gmail.com>
2024-01-07 08:54:13 -0800

    tf tokenizer

---
+++ NLP/tf_tokenizer.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+learn_text = [
+    'Weather is great today',
+    'Weather was great yesterday'
+    'Weather is great tomorrow'
+    ]
+
+tokenizer = Tokenizer(num_words = 100)
+tokenizer.fit_on_texts(learn_text)
+word_index = tokenizer.word_index
+
+print(word_index)
